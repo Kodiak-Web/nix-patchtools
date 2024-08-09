978eb4162174d155e434f18251d3e9545ca24368
kodiakweb <kodiakweb@proton.me>
2024-08-09 08:25:37 -0800

    test if sed newlines cause build syntax error

--- default.nix
+++ default.nix
@@ -5,9 +5,7 @@ stdenv.mkDerivation {
   buildCommand = ''
     mkdir -p $out/bin
     cp ${./autopatchelf} $out/bin/autopatchelf
-    sed -i \
-      -e "s|file |${file}/bin/file |" \
-      -e "s|getopt |${getopt}/bin/getopt |" $out/bin/autopatchelf
+    sed -i -e "s|file |${file}/bin/file |" -e "s|getopt |${getopt}/bin/getopt |" $out/bin/autopatchelf
     chmod +x $out/bin/autopatchelf
     patchShebangs $out/bin
   '';
